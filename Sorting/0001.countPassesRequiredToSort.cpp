65cdd0156a0c30e65d6b72962e9e3003ed235727
VirajYan <virrrajyan@gmail.com>
2025-03-05 01:13:20 +0530

    passes added

---
+++ Sorting/0001.countPassesRequiredToSort.cpp
@@ -0,0 +1,44 @@
+// here if the array is already sorted/not sorted it still runs O(n^2)
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> bubbleSort(vector<int> &nums)
+    {
+        bool swapped; // pehele true rakho
+        int passCount = 0;
+        for (int i = 0; i <= nums.size() - 1; i++)
+        {
+            passCount++;
+            swapped = false; // ab false set kr do
+            for (int j = 0; j < nums.size() - i - 1; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    swap(nums[j], nums[j + 1]);
+                    swapped = true; // agar swap hua to hi true krna hai
+                }
+            }
+            if (!swapped)
+                break; // agar poore j me true hua hi nahi, mtlab swap hua hi nahi to break kr do
+        }
+        cout << "total passes required : " << passCount << endl;
+        return nums;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5};
+    Solution s;
+    vector<int> ans = s.bubbleSort(nums);
+
+    for (int num : ans)
+    {
+        cout << num << " ";
+    }
+    return 0;
+}
