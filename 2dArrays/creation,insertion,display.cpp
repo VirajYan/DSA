2409e222c7b4ff580db7e8b2613dda6344c5ff44
VirajYan <virrrajyan@gmail.com>
2025-03-21 12:01:48 +0530

    updated

--- 2dArrays/creation,insertion,display.cpp
+++ 2dArrays/creation,insertion,display.cpp
@@ -6,9 +6,20 @@ using namespace std;
 int main()
 {
     int arr[3][4];
+    /*row wise input
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    */
+
+    // col wise input
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
         {
             cin >> arr[i][j];
         }
