e94e245414c459ad52743e4a5c05dd599aa44e33
VirajYan <virrrajyan@gmail.com>
2025-04-09 12:59:26 +0530

    insert at position

---
+++ LinkedList/0003.insertAtPosition.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAthead(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    Node *newNode = new Node(data);
+    tail->next = newNode;
+    tail = newNode;
+}
+
+int findLength(Node *&head)
+{
+    int len = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        len++;
+    }
+    return len;
+}
+void insertAtPosition(int data, int position, Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    if (position == 0)
+    {
+        insertAthead(head, tail, data);
+        return;
+    }
+    int len = findLength(head);
+    if (position >= len)
+    {
+        insertAtTail(head, tail, data);
+        return;
+    }
+
+    int i = 1;
+    Node *prev = head;
+    while (i < position)
+    {
+        prev = prev->next;
+        i++;
+    }
+    Node *curr = prev->next;
+
+    Node *newNode = new Node(data);
+
+    newNode->next = curr;
+
+    prev->next = newNode;
+}
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+    insertAthead(head, tail, 50);
+    insertAthead(head, tail, 70);
+    insertAthead(head, tail, 80);
+
+    print(head);
+    cout << endl;
+
+    insertAtPosition(101, 2, head, tail);
+
+    print(head);
+
+    return 0;
+}
