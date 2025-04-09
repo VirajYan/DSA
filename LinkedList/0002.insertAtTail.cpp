5502bd777a8d34e52ff2d6c2c59aac4263710112
VirajYan <virrrajyan@gmail.com>
2025-04-09 12:21:31 +0530

    insert at tail

---
+++ LinkedList/0002.insertAtTail.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    Node *newNode = new Node(data);
+    tail->next = newNode;
+    tail = newNode;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = new Node(10);
+    insertAtTail(head, tail, 60);
+
+    print(tail); // because we want to print tail right
+
+    return 0;
+}
